158137e0044f01e3fcf0648e66a9674b56e2af98
Łukasz Dziedzic <dziedzic@alfabety.pl>
2019-05-08 07:47:18 +0200

    Initial Commit

---
+++ EdgesFramework/EdgesFramework.h
@@ -0,0 +1,19 @@
+//
+//  EdgesFramework.h
+//  EdgesFramework
+//
+//  Created by Łukasz Dziedzic on 08/05/2019.
+//  Copyright © 2019 Łukasz Dziedzic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EdgesFramework.
+FOUNDATION_EXPORT double EdgesFrameworkVersionNumber;
+
+//! Project version string for EdgesFramework.
+FOUNDATION_EXPORT const unsigned char EdgesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EdgesFramework/PublicHeader.h>
+
+
